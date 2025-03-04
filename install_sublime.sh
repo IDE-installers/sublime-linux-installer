add23a93b00fe8c61dfcdabb53231ce228e1190d
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2025-03-04 15:53:05 +0100

    Remove duplicate 'sudo'

--- install_sublime.sh
+++ install_sublime.sh
@@ -49,7 +49,7 @@ function install(){
             sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 
             sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-            sudo sudo dnf install sublime-text -y
+            sudo dnf install sublime-text -y
             ;;
 
         yum)
